1e40edc4aad0a1f0c797215e80f1e1b7678bb236
Joachim Danielsen <dev.joda@gmail.com>
2025-01-07 12:48:43 +0100

    update config.js

--- src/config.js
+++ src/config.js
@@ -1,32 +1,3 @@
-const gameStates = {
-  NOT_STARTED: 'notStarted',
-  IN_PROGRESS: 'inProgress',
-  COMPLETED: 'completed',
-}
-
-const categories = [
-  { value: '', label: 'Any Category' },
-  { value: '11', label: 'Movies' },
-  { value: '14', label: 'Television' },
-  { value: '14', label: 'Video Games' },
-  { value: '18', label: 'Computers' },
-  { value: '19', label: 'Mathematics' },
-  { value: '20', label: 'Mythology' },
-  { value: '22', label: 'Geographics' },
-  { value: '21', label: 'Sports' },
-  { value: '23', label: 'History' },
-  { value: '24', label: 'Politics' },
-  { value: '26', label: 'Celebrities' },
-  { value: '28', label: 'Vehicles' },
-].sort((a, b) => a.label.localeCompare(b.label))
-
-const difficulties = [
-  { value: '', label: 'Any Difficulty'},
-  { value: 'easy', label: 'Easy'},
-  { value: 'medium', label: 'Medium'},
-  { value: 'hard', label: 'Hard'},
-]
-
 const config = {
   gameStates: {
     NOT_STARTED: 'notStarted',
